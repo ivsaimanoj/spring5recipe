9b9d3e19caaf2d0f1903993a89acd63a498c1fa8
Venkata Sai Manoj Illendula <ivsaimanoj@gmail.com>
2017-12-31 13:35:36 -0600

    ManytoMany and H2 connections

--- spring5recipe/src/main/java/com/vism/spring5recipe/domain/Recipe.java
+++ spring5recipe/src/main/java/com/vism/spring5recipe/domain/Recipe.java
@@ -3,15 +3,20 @@ package com.vism.spring5recipe.domain;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+@Entity
 public class Recipe {
 
 	@Id
@@ -37,6 +42,12 @@ public class Recipe {
 
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
+	
+	@ManyToMany
+	@JoinTable(name = "recipe_category", joinColumns = 
+				@JoinColumn(name = "recipe_id"), inverseJoinColumns=@JoinColumn(name = "category_id")
+			)
+	private Set<Category> categories;
 
 	public Long getId() {
 		return id;
