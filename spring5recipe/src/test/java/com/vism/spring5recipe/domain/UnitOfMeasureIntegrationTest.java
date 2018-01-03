707cd4ec756d4e6be8896f43f8f451f3aa444ddc
Venkata Sai Manoj Illendula <ivsaimanoj@gmail.com>
2018-01-03 11:45:03 -0600

    using integration test with spring and using dirties context

---
+++ spring5recipe/src/test/java/com/vism/spring5recipe/domain/UnitOfMeasureIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.vism.spring5recipe.domain;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.vism.spring5recipe.repositories.UnitOfMeasureRepository;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class UnitOfMeasureIntegrationTest {
+
+	@Autowired
+	UnitOfMeasureRepository uomRepo;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+//	@DirtiesContext --> this will reload the spring context
+	public void findByDescriptionTest() {
+		Optional<UnitOfMeasure> uomOptional = uomRepo.findByDescription("Teaspoon");
+		assertEquals("Teaspoon", uomOptional.get().getDescription());
+	}
+
+	
+	@Test
+	public void findByDescriptionTest2() {
+		Optional<UnitOfMeasure> uomOptional = uomRepo.findByDescription("Cup");
+		assertEquals("Cup", uomOptional.get().getDescription());
+	}
+
+}
